ebb7d66dc63f9dbfc84cb2392bb9923de78c8e03
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-31 01:20:02 +0600

    jeson placeholder post api call

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('Hello api')
+const loadPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => displayPost(data))
+}
+
+loadPost()
+const displayPost = (posts) => {
+    const postContiner = document.getElementById('post-continer')
+    for (post of posts) {
+        const div = document.createElement('div')
+        div.classList.add('post')
+        div.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+
+        `
+        postContiner.appendChild(div)
+    }
+}
+
